fd26c302c1e55c4b876009eaf6e5e8bd2ee49d74
Yang <yangcjcjlu@126.com>
2023-09-20 11:44:33 +0800

    build/release

---
+++ dist/assets/index.1b05c30e.js
@@ -0,0 +1,5 @@
+import{T as r}from"./index.56607e43.js";import"./index.91378ad2.js";import"./formModel.a6a83887.js";/**
+ * tdesign v1.2.4
+ * (c) 2023 tdesign
+ * @license MIT
+ */var i=r;export{i as T};
--- dist/assets/index.c00802b1.js
+++
@@ -1,5 +0,0 @@
-import{U as a}from"./index.aab392cb.js";import"./index.3351b187.js";/**
- * tdesign v1.2.4
- * (c) 2023 tdesign
- * @license MIT
- */var p=a;export{p as U};
